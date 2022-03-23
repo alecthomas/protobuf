87df69cc9ae6d349f7e3808179e6a99f5a947e39
Cam Hutchison <camh@xdna.net>
2022-03-23 19:10:16 +1100

    parser: Fix successive numeric escapes, rune vs byte
    Fix unquoting of successive numeric escape sequences so that the second
    one straight after the first is actually processed as an escape sequence
    and not literal chars.
    
    Limit the length of numeric sequences to 2 or 3 whether hex or octal,
    and emit an error if the octal value is too large.
    
    Write numeric escapes as bytes not runes as non-ascii byte values are
    interpreted differently as runes and becomes incorrect.

--- parser/unquote_test.go
+++ parser/unquote_test.go
@@ -18,10 +18,13 @@ func TestUnquote(t *testing.T) {
 		{`"\n\x17"`, "\n\027"},
 		{`"hello\0world"`, "hello\000world"},
 		{`"hello\x0world"`, "hello\000world"},
+		{`"\0001"`, "\0001"},
+		{`"\x001"`, "\x001"},
+		{`"\341\210\264"`, "ሴ"},
 	}
 	for _, test := range tests {
 		actual, err := unquote(lexer.Token{Value: test.input})
 		require.NoError(t, err)
-		require.Equal(t, actual.Value, test.expected)
+		require.Equal(t, test.expected, actual.Value)
 	}
 }
